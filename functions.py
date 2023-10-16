98c497102db661da4365103c33fc5c43818197fc
HasanChamok <hasan.chamok16@gmail.com>
2023-10-16 15:03:06 +0600

    changes

--- functions.py
+++ functions.py
@@ -18,17 +18,17 @@ def yolo(img_path):
   img=cv2.imread(img_path)
   img=cv2.resize(img, (416,416))
   results = model(img)
-  print(results.pandas().xyxy[0])
+  print(results.pandas().xyxy)
   Label,Bbox,Confidence=[],[],[]
   for res in results.pandas().xyxy:
       # print(len(res))
       for obj in range(len(res)):
-          if res['confidence'][obj]>0.2:
-              (x1, y1, x2, y2) = (res['xmin'][obj],res['ymin'][obj],res['xmax'][obj],res['ymax'][obj])
-              bbox=(x1,y1,x2,y2)
-              className = classes[res['class'][obj]]
-              Label.append(className)
-              Bbox.append(bbox)
-              Confidence.append(res['confidence'][obj])
+          if res['confidence'][obj]>0.1:
+            (x1, y1, x2, y2) = (res['xmin'][obj],res['ymin'][obj],res['xmax'][obj],res['ymax'][obj])
+            bbox=(x1,y1,x2,y2)
+            className = classes[res['class'][obj]]
+            Label.append(className)
+            Bbox.append(bbox)
+            Confidence.append(res['confidence'][obj])
             #   cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (250, 19, 10),2)
   return Label,Bbox,Confidence
